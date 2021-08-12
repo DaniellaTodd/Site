c1193e9a8d38953602ce02639b42697e32def8c7
Daniella Todd <daniella.todd@hotmail.com>
2021-08-12 21:14:10 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 const panoImage = document.querySelector('.pano-image');
-const img = 'images/image2.jpeg';
+const img = 'images/pano.jpeg';
 
 const panorama = new PANOLENS.ImagePanorama(img);
 const viewer = new PANOLENS.Viewer({
   container: panoImage,
+  horizontalView: true,
 });
 
 viewer.add(panorama);
