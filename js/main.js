d0b5ddb2f24f93e764e8569c5cb33f1ecc578d2d
Daniella Todd <daniella.todd@hotmail.com>
2021-08-15 15:10:21 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ const testPano = 'images/image2.jpeg';
 const panorama = new PANOLENS.ImagePanorama(testPano);
 const viewer = new PANOLENS.Viewer({
   container: panoImage,
+  horizontalView: true,
+  // autoRotate: true,
+  // autoRotateSpeed: 2,
 });
 
 viewer.add(panorama);
+
+// const infospot = new PANOLENS.Infospot(300, PANOLENS.DataImage.Info)
+// infospot.position.set(-900,-700,-5000)
+// // infospot.position.set(0,0,0)
+// infospot.addHoverText('Profane Muffling', 40)
+// panorama.add(infospot)
