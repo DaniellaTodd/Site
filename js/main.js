78a496c2f5f1079091e28e1ffba0a6925d6d97a2
Daniella Todd <daniella.todd@hotmail.com>
2021-08-17 15:03:46 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const panoImage = document.querySelector('.pano-image');
-const testPano = 'images/image2.jpeg';
+const img = 'images/image2.jpeg';
 
-const panorama = new PANOLENS.ImagePanorama(testPano);
+const panorama = new PANOLENS.ImagePanorama(img);
+panorama.position.set(0,0,1000)
 const viewer = new PANOLENS.Viewer({
   container: panoImage,
   horizontalView: true,
@@ -10,6 +11,44 @@ const viewer = new PANOLENS.Viewer({
 });
 
 viewer.add(panorama);
+
+function togglePopup(){
+  document.getElementById("popup-1").classList.toggle("active");
+}
+function togglePopupTwo(){
+  document.getElementById("popup-2").classList.toggle("active");
+}
+
+// function playclip() {
+// if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+// if (document.all)
+//  {
+//   document.all.sound.src = "waves.mp3";
+//  }
+// }
+//
+// else {
+// {
+// var audio = document.getElementsByTagName("audio")[0];
+// audio.play();
+// }
+// }
+// }
+
+var audio = document.getElementById("myAudio");
+
+function playAudio() {
+  audio.play();
+  document.querySelector(".pause-hide").style.display = "block";
+  document.querySelector(".play-hide").style.display = "none";
+}
+
+
+function pauseAudio() {
+  audio.pause();
+  document.querySelector(".pause-hide").style.display = "none";
+  document.querySelector(".play-hide").style.display = "block";
+}
 
 // const infospot = new PANOLENS.Infospot(300, PANOLENS.DataImage.Info)
 // infospot.position.set(-900,-700,-5000)
