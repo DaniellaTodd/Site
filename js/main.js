5f0e367ff6381a62153e580acc220d3d1b26c9e5
Daniella Todd <daniella.todd@hotmail.com>
2021-08-18 15:14:44 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,11 +2,10 @@ const panoImage = document.querySelector('.pano-image');
 const img = 'images/image2.jpeg';
 
 const panorama = new PANOLENS.ImagePanorama(img);
-panorama.position.set(0, 0, -1000)
+// panorama.position.set(0, 0, -1000)
 const viewer = new PANOLENS.Viewer({
   container: panoImage,
   horizontalView: true,
-  viewIndicator: true,
   controlBar: false,
   // initialLookPosition: new THREE.Vector3( -5000.00, 167.06, 3449.90 ),
 });
@@ -16,9 +15,9 @@ viewer.add(panorama);
 function togglePopup(){
   document.getElementById("popup-1").classList.toggle("active");
 }
-function togglePopupTwo(){
-  document.getElementById("popup-2").classList.toggle("active");
-}
+// function togglePopupTwo(){
+//   document.getElementById("popup-2").classList.toggle("active");
+// }
 
 
 
